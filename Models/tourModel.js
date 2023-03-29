3c1aab632032e3d7247a8ffa4c47ec144c484b18
Shivam <shivampandey88@hotmail.com>
2023-03-29 17:18:46 +0530

    Virtual properties and mongoose middleware

--- Models/tourModel.js
+++ Models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
     {
@@ -67,8 +68,54 @@ const tourSchema = new mongoose.Schema(
           type: Boolean,
           default: false
         }
+      },
+      {                               //We can not only pass our schema design but also optional virtual propery
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
       }
 )
+
+tourSchema.virtual('durationWeeks').get(function() {
+  return this.duration / 7;
+});
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function(next) {
+//   console.log('Will save document...');
+//   next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
+ 
+// QUERY MIDDLEWARE
+// tourSchema.pre('find', function(next) {
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this.pipeline());
+  next();
+});
 
 const Tour = mongoose.model("Tour", tourSchema)         
 // This is a model which will be created from the schema design/rules
