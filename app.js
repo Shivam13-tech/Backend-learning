401ae6e4c4ebe14146a24795639e036254ce0e75
Shivam <shivampandey88@hotmail.com>
2023-04-13 19:02:28 +0530

    Implementing more advance security measures

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
 const express = require('express')
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const AppError = require("./Utils/appError")
 const globalErrorhandler = require("./Controllers/errorController")
 const tourRouter = require("./Routes/tourRoutes")
 const userRouter = require("./Routes/userRoutes")
 
 const app = express()
-app.use(express.json())        // This middleware is important to add incoming response data
+
+app.use(helmet());           //Securing http headers
+
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+  });
+app.use('/api', limiter);      //Rate limiting for brute force attack
+
+app.use(express.json({ limit: '10kb' }))        // This middleware is important to add incoming response data
+//Limit here to handle incoming data to avoid unwanted huge datasets
 
 
+//After the data comes to us:
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());          //discards symbols and dots in input field
+
+// Data sanitization against XSS
+app.use(xss());   // gets rid of html+js code if someone tries
+
+
+
+// Prevent parameter pollution
+app.use(
+    hpp({
+      whitelist: [
+        'duration',
+        'ratingsQuantity',
+        'ratingsAverage',
+        'maxGroupSize',
+        'difficulty',
+        'price'
+      ]
+    })
+  );
 //////////////// Tour resources //////////////
 
 app.use("/api/v1/tours", tourRouter)        // tourRouter works as our middleware for request response cycle when user perform action
