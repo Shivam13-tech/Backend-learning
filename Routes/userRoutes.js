bf3817c82722ecd3b7e80ddfc7a4ea0dac715946
Shivam <shivampandey88@hotmail.com>
2023-04-20 18:19:08 +0530

    Getting logged in user details

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -9,6 +9,7 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
+router.get('/me', authController.protect,userController.getMe, userController.getUserByID)
 router.patch('/updateMyPassword',authController.protect,authController.updatePassword);
 router.patch('/updateMe', authController.protect, userController.updateMe);
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
